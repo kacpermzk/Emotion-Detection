e4e2834e2cd63c09f012f118b2c5138f2a777f8e
kacpermzk <107047796+kacpermzk@users.noreply.github.com>
2023-07-08 13:33:07 +0200

    Add files via upload

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ PyPower Projects
 Emotion Detection Using AI
 '''
 
-#USAGE : python test.py
+#How to Use: RUN test.py
 
 from keras.models import load_model
 from time import sleep
@@ -11,6 +11,7 @@ from keras.preprocessing.image import img_to_array
 from keras.preprocessing import image
 import cv2
 import numpy as np
+import tensorflow as tf 
 
 face_classifier = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
 classifier =load_model('./Emotion_Detection.h5')
@@ -26,10 +27,12 @@ while True:
     ret, frame = cap.read()
     labels = []
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    #model changes to gray
     faces = face_classifier.detectMultiScale(gray,1.3,5)
 
     for (x,y,w,h) in faces:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+        #colour of rectangle that is drawn
         roi_gray = gray[y:y+h,x:x+w]
         roi_gray = cv2.resize(roi_gray,(48,48),interpolation=cv2.INTER_AREA)
 
@@ -53,6 +56,7 @@ while True:
         print("\n\n")
     cv2.imshow('Emotion Detector',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
+        #press q to quit
         break
 
 cap.release()
--- train.py
+++ train.py
@@ -2,7 +2,6 @@ from keras.applications import MobileNet
 from keras.models import Sequential,Model 
 from keras.layers import Dense,Dropout,Activation,Flatten,GlobalAveragePooling2D
 from keras.layers import Conv2D,MaxPooling2D,ZeroPadding2D
-from keras.layers.normalization import BatchNormalization
 from keras.preprocessing.image import ImageDataGenerator
 
 # MobileNet is designed to work with images of dim 224,224
